46cdeede4c1e6c33c9c4a227feb874b3c69ccaee
Karlo Šebalj <ks049890@fsb.hr>
2020-01-04 14:33:10 +0100

    The This Pointer

--- main.cpp
+++ main.cpp
@@ -1,35 +1,84 @@
 #include <iostream>
 #include <string>
 
-class A
+class Point2D
 {
 public:
-	enum MyEnumType 
-	{
-		MET_A1 = 0,
-		MET_A2,
-		MET_A3
-	};
+	Point2D();
+	Point2D(int x, int y);
+	void SetX(int x);
+	void SetY(int y);
+	int GetX();
+	int GetY();
+	void Display();
 
-	A();
-	A(MyEnumType type);
+	Point2D& RefOurselves();
+	Point2D* PointerOurselves();
+
 private:
-	MyEnumType mMET;
+	int mX;
+	int mY;
 };
 
-A::A() :A(MyEnumType::MET_A1)
+Point2D::Point2D(): Point2D(0,0)
 {
+	mX = 0;
+	mY = 0;
 }
 
-A::A(MyEnumType type) : mMET(type)
+Point2D::Point2D(int x, int y) : mX(x), mY(y)
 {
-
+	Display();
+	this->Display(); //Does the same thing
+	mX = 0;
+	this->mX = 0; //Does the same thing as above
 }
 
-int main(int argc, const char *argv[])
+Point2D& Point2D::RefOurselves()
 {
-	A::MyEnumType type = A::MyEnumType::MET_A3;
-	A myA = A(type);
+	return *this; //gets value of this
+}
+
+Point2D* Point2D::PointerOurselves()
+{
+	return this;
+}
+
+void Point2D::SetX(int x)
+{
+	mX = x;
+}
+
+void Point2D::SetY(int y)
+{
+	mX = y;
+}
+
+int Point2D::GetX()
+{
+	return mX;
+}
+
+int Point2D::GetY()
+{
+	return mY;
+}
+
+void Point2D::Display()
+{
+	std::cout << "X: " << mX << ", Y: " << mY << std::endl;
+}
+
+int main()
+{
+	Point2D point(5, 10);
 	
+	Point2D& refPoint = point.RefOurselves();
+	Point2D* ptrPoint = point.PointerOurselves();
+
+	refPoint.Display();
+	ptrPoint->Display();
+
+	std::cout << "Point's address: " << &point << " refPoint is referencing: " << &refPoint << " ptrPoint is pointing to: " << ptrPoint << std::endl;
 	system("pause");
 }
