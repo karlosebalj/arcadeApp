8dc16b5b2be5bba894db461b7e9d227df61bfd55
Karlo Sebalj <karlo.sebalj@greyp.com>
2020-11-06 14:59:06 +0100

    Section 2.8 - Classes and Objects

---
+++ sections/Section2_get_to_class/2.8_classes_and_objects.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Point2D {
+public:
+  void Set(int dx, int dy);
+  void Display();
+
+private:
+  int mX;
+  int mY;
+
+};
+
+void Point2D::Set(int dx, int dy) {
+  mX = dx;
+  mY = dy;
+}
+
+void Point2D::Display() {
+  std::cout << "X: " << mX << ", Y: " << mY << std::endl;
+}
+
+int main() {
+  Point2D point;
+  point.Set(10, 5);
+  point.Display();
+
+  Point2D point2;
+  point2.Set(3, 8);
+  point2.Display();
+
+  return 0;
+}
