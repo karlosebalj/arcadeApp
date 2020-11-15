11d3e8faaf8f38ccde1dff80325722ac3d0e96f5
Karlo Sebalj <karlo.sebalj@greyp.com>
2020-11-15 11:14:21 +0100

    Section 2.12 - struct vs class

---
+++ sections/Section2_get_to_class/2.12_struct_vs_class.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct SPoint2D {
+  void SetX(int x);
+  void SetY(int y);
+  int GetX();
+  int GetY();
+  void Display();
+  int mX;
+  int mY;
+
+};
+
+class CPoint2D {
+ public:
+  void SetX(int x);
+  void SetY(int y);
+  int GetX();
+  int GetY();
+  void Display();
+
+ private:
+  int mX;
+  int mY;
+};
+
+int main() {
+  SPoint2D structPoint;
+  CPoint2D classPoint;
+  structPoint.SetX(0); // works
+  classPoint.SetX(0); // will not work
+
+  return 0;
+}
