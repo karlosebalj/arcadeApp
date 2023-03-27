58c7d7500c5ca854e2b09050a8b20ce6c1d89314
Karlo Sebalj <karlo.sebalj@greyp.com>
2023-03-27 18:33:20 +0200

    3.20_reference_in_a_class

---
+++ Section3_the_particulars/3.20_reference_in_a_class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class A {
+ public:
+  A() : A(0) {}
+  A(int x) : mX(x) {}
+
+  int mX;
+};
+
+class B {
+ public:
+  B(A& a);
+  void Display() { std::cout << mARef.mX << std::endl; }
+
+ private:
+  A& mARef;
+};
+
+B::B(A& a) : mARef(a) {}
+
+int main() {
+  A myA(10);
+  B myB(myA);
+  myB.Display();
+  return 0;
+}
