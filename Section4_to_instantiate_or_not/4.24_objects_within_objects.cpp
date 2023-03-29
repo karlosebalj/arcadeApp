26dc92b80879201aece64d103c3781ea3420cb39
Karlo Sebalj <karlo.sebalj@greyp.com>
2023-03-29 10:53:23 +0200

    4.24_objects_within_objects

---
+++ Section4_to_instantiate_or_not/4.24_objects_within_objects.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+class B {
+ public:
+  B(int value) : mValue(value) {}
+  int Value() { return mValue; }
+  void SetValue(int value) { mValue = value; }
+
+ private:
+  int mValue;
+};
+
+class A {
+ public:
+  A() : mB(0) {}
+  ~A() {}
+
+  void SetValue(int value) { mB.SetValue(value); }
+
+  void Display() { std::cout << "A's value is: " << mB.Value() << std::endl; }
+
+ private:
+  B mB;
+};
+
+int main() { return 0; }
